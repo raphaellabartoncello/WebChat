a9174c89b52361c93d3ca96000b3002ba8cb6876
Raphaella Bartoncello <raphaella.bartoncello@gmail.com>
2020-06-22 19:36:55 -0300

    Add project files.

---
+++ WebChat/Db/ChatDb.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebChat.Models;
+
+namespace WebChat.Db
+{
+    public class ChatDb
+    {
+        private const string chaveChat = "chat";
+        private const string chaveparticipantes = "participantes";
+        private const string adminNome = "admin";
+        private const string adminMensagemInicial = "Chat Iniciado com sucesso";
+
+        //
+        // Iniciar: Cria os itens da aplicação
+        //
+        internal static void Iniciar()
+        {
+            // Lista de Mensagens
+            var lista = new List<ChatMensagem>();
+            HttpContext.Current.Application.Add(chaveChat, lista);
+            // Lista de Participantes On-Line
+            var participantes = new List<string>();
+            HttpContext.Current.Application.Add(chaveparticipantes, participantes);
+            participantes.Add(adminNome);
+        }
+
+    }
+
+}
---
+++ WebChat/Models/ChatMensagem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebChat.Models
+{
+    public class ChatMensagem
+    {
+        public string Participante { get; set; }
+        public DateTime DataHora { get; set; }
+        public string Mensagem { get; set; }
+    }
+}
